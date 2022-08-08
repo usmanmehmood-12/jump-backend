72ed3e8e9d27190b19592a72b7b59cae3ce4f323
usmanmehmood-12 <usmanmehmood12@gmail.com>
2022-08-09 00:44:57 +0400

    add index/ project entry point

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { ttlStatusUpdate } = require('./service/TTLService');
+const url = 'mongodb://localhost/testDB'
+
+const app = express();
+app.use(express.json())
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+con.on('open', () => {
+  console.log('Connected..')
+})
+
+try {
+
+  ttlStatusUpdate();
+
+} catch (error) {
+  console.log('error in scheduler', error)
+}
+
+require('./routers/index')(app)
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Jump backend task listening at http://localhost:${port}`);
+});
